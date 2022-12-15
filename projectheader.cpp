784cc6ea5a4d83c8436c2ee4b98d6fd0432d25f2
Asad ALI <asadalimcj@gmail.com>
2022-12-15 18:53:30 +0500

    this is my assignment

---
+++ projectheader.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<"************************************************"<<endl;
+cout<<"*                                               *"<<endl;
+cout<<"*                                               *"<<endl;
+cout<<"*          WELCOME TO SPORTS                    *"<<endl;
+cout<<"*                 GALA                          *"<<endl;
+cout<<"*                                               *"<<endl;
+cout<<"*                                               *"<<endl;
+cout<<"*                                               *"<<endl;
+cout<<"************************************************"<<endl;
+
+
+
+
+
+}
